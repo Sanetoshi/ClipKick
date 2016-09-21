dbf143ebaec897d1a982fa179e080dcb6843c0fd
Sanetoshi <sanetoshi.dayone@gmail.com>
2016-09-21 11:22:22 +0900

    check executable

--- ClipKick/Program.cs
+++ ClipKick/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -8,7 +9,8 @@ namespace ClipKick
     {
         static string target = string.Empty;
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             // クリップボードチェック
             var t = new System.Threading.Thread(ClipKick.GetClipboardText);
             t.SetApartmentState(System.Threading.ApartmentState.STA);
@@ -19,12 +21,21 @@ namespace ClipKick
             var cmds = Environment.GetCommandLineArgs();
 
             // 引数およびターゲット文字列なしの場合は何もしない
-            if (cmds.Count() < 1 && string.IsNullOrEmpty(target)) {
+            if (cmds.Count() < 2 || string.IsNullOrEmpty(target)) {
                 return;
             }
 
             // 実行プログラム名確定
             var strProgram = cmds[1];
+            if (ClipKick.CheckFile(strProgram)) {
+                var ext = Path.GetExtension(strProgram).ToLower();
+                if (ext.Equals("exe") == false && ext.Equals("bat") == false) {
+                    return;
+                }
+            } else {
+                return;
+
+            }
 
             // 引数の強制ディクレクトリ指定フラグ
             var bForceDir = false;
@@ -70,18 +81,21 @@ namespace ClipKick
             p.Start();
         }
 
-        static void GetClipboardText() {
+        static void GetClipboardText()
+        {
             if (Clipboard.ContainsText()) {
                 target = Clipboard.GetText();
             }
         }
 
-        static bool CheckFile(string path) {
-            return System.IO.File.Exists(path);
+        static bool CheckFile(string path)
+        {
+            return File.Exists(path);
         }
 
-        static bool CheckAndModifyDirectory(ref string path) {
-            if (System.IO.Directory.Exists(path)) {
+        static bool CheckAndModifyDirectory(ref string path)
+        {
+            if (Directory.Exists(path)) {
                 if (path.EndsWith("\\") == false) {
                     path += "\\";
                 }
